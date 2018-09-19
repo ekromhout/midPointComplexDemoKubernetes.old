5fa66bd1357c4a8f0d489e8f9fbc2acd80753b8b
Pavol Mederly <mederly@evolveum.com>
2018-09-20 00:14:35 +0200

    Minor naming fixes in midPoint dockerization

--- midpoint/midpoint-server/container_files/usr-local-bin/entrypoint.sh
+++ midpoint/midpoint-server/container_files/usr-local-bin/entrypoint.sh
@@ -5,8 +5,8 @@ export ENV=${ENV//[; ]/_}
 export USERTOKEN=${USERTOKEN//[; ]/_}
 
 echo "Linking secrets and config files; using authentication: $AUTHENTICATION"
-ln -sf /run/secrets/shib_sp-key.pem /etc/shibboleth/sp-key.pem
-ln -sf /run/secrets/host-key.pem /etc/pki/tls/private/host-key.pem
+ln -sf /run/secrets/m_sp-key.pem /etc/shibboleth/sp-key.pem
+ln -sf /run/secrets/m_host-key.pem /etc/pki/tls/private/host-key.pem
 ln -sf /etc/httpd/conf.d/midpoint.conf.auth.$AUTHENTICATION /etc/httpd/conf.d/midpoint.conf
 
 httpd-shib-foreground &
