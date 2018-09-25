a084cfca1a0b45ed632a77540f43e22da45c07cc
Pavol Mederly <mederly@evolveum.com>
2018-09-25 17:24:41 +0200

    Fix healthcheck mistake

--- midpoint/midpoint-server/container_files/usr-local-bin/healthcheck.sh
+++ midpoint/midpoint-server/container_files/usr-local-bin/healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(set -o pipefail ; curl -k -f http://localhost:443/midpoint/actuator/health | tr -d '[:space:]' | grep -q "\"status\":\"UP\"") || exit 1
+(set -o pipefail ; curl -k -f https://localhost:443/midpoint/actuator/health | tr -d '[:space:]' | grep -q "\"status\":\"UP\"") || exit 1
