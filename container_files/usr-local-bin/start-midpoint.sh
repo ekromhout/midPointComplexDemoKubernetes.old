ceafa77a75096781e1e9191372f7f08ebe567754
Pavol Mederly <mederly@evolveum.com>
2018-10-01 17:07:51 +0200

    Add auto-upgrade support

--- container_files/usr-local-bin/start-midpoint.sh
+++ container_files/usr-local-bin/start-midpoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$AUTHENTICATION" = "shibboleth" ]; then
-  LOGOUT_URL_DIRECTIVE="-Dauth.logout.url=$LOGOUT_URL"
-else
-  LOGOUT_URL_DIRECTIVE=
-fi
-
 java -Xmx$MEM -Xms2048m -Dfile.encoding=UTF8 \
        -Dmidpoint.home=$MP_DIR/var \
        -Dmidpoint.repository.database=$REPO_DATABASE_TYPE \
@@ -13,7 +7,10 @@ java -Xmx$MEM -Xms2048m -Dfile.encoding=UTF8 \
        -Dmidpoint.repository.jdbcPassword_FILE=$REPO_PASSWORD_FILE \
        -Dmidpoint.repository.jdbcUrl="`$MP_DIR/repository-url`" \
        -Dmidpoint.repository.hibernateHbm2ddl=none \
-       -Dmidpoint.repository.missingSchemaAction=create \
+       -Dmidpoint.repository.missingSchemaAction=$REPO_MISSING_SCHEMA_ACTION \
+       -Dmidpoint.repository.upgradeableSchemaAction=$REPO_UPGRADEABLE_SCHEMA_ACTION \
+       $(if [ -n "$REPO_SCHEMA_VERSION_IF_MISSING" ]; then echo "-Dmidpoint.repository.schemaVersionIfMissing=$REPO_SCHEMA_VERSION_IF_MISSING"; fi) \
+       $(if [ -n "$REPO_SCHEMA_VARIANT" ]; then echo "-Dmidpoint.repository.schemaVariant=$REPO_SCHEMA_VARIANT"; fi) \
        -Dmidpoint.repository.initializationFailTimeout=60000 \
        -Dmidpoint.keystore.keyStorePassword_FILE=$KEYSTORE_PASSWORD_FILE \
        -Dmidpoint.logging.alt.enabled=true \
@@ -21,7 +18,7 @@ java -Xmx$MEM -Xms2048m -Dfile.encoding=UTF8 \
        -Dmidpoint.logging.alt.timezone=UTC \
        -Dspring.profiles.active="`$MP_DIR/active-spring-profiles`" \
        -Dauth.sso.header=$SSO_HEADER \
-       $LOGOUT_URL_DIRECTIVE \
+       $(if [ "$AUTHENTICATION" = "shibboleth" ]; then echo "-Dauth.logout.url=$LOGOUT_URL"; fi) \
        -Dserver.tomcat.ajp.enabled=$AJP_ENABLED \
        -Dserver.tomcat.ajp.port=$AJP_PORT \
        -Dlogging.path=/tmp/logtomcat \
